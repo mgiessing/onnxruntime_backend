e9150082ba9d348976629fdb36399f5d9515d41d
Marvin Giessing <marvin.giessing@gmail.com>
2022-04-06 23:59:55 +0200

    Ppc64le stub

--- tools/gen_ort_dockerfile.py
+++ tools/gen_ort_dockerfile.py
@@ -92,19 +92,12 @@ RUN apt-get update && apt-get install -y --no-install-recommends \
         python3-pip \
         git \
         gnupg \ 
-        gnupg1
+        gnupg1 \
+	make \
+	cmake
 
 # Install dependencies from
 # onnxruntime/dockerfiles/scripts/install_common_deps.sh.
-RUN wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | \
-      gpg --dearmor - |  \
-      tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null && \
-    apt-add-repository 'deb https://apt.kitware.com/ubuntu/ focal main' && \
-    apt-get update && \
-    apt-get install -y --no-install-recommends \
-      cmake-data=3.21.1-0kitware1ubuntu20.04.1 cmake=3.21.1-0kitware1ubuntu20.04.1 && \
-    cmake --version
-
 '''
     if FLAGS.enable_gpu:
         df += '''
